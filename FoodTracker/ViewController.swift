2f8496c82b5f480ea5e3ffa8419e81ea35b52a54
kenichi04 <slow_life_1407@yahoo.co.jp>
2020-12-30 18:17:57 +0900

    UITextFieldDelegateメソッド追加

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
 
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
+}
+
+// MARK: UITextFieldDelefate
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // First Responderが解除された後に呼ばれる
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
 }
 
